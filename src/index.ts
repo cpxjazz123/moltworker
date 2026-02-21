45c1dccba6db0aae1f31b5f92f595fdc56a4d4dc
wlia0047 <wlia0047@student.monash.edu>
2026-02-21 22:49:52 +1100

    Restore original worker name and config

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ function transformErrorMessage(message: string, host: string): string {
   return message;
 }
 
-export { Sandbox, Sandbox as MoltbotSandboxV2 };
+export { Sandbox, Sandbox as MoltbotSandbox };
 
 /**
  * Validate required environment variables.
